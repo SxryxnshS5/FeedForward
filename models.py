e43197ae28922d7ac1576c68d1ca7abf6b5ef9c8
alexlines8 <alexlines8@yahoo.co.uk>
2024-05-10 13:33:44 +0100

    Create User class and constructor

--- models.py
+++ models.py
@@ -1,1 +1,15 @@
+"""python file that contains all the models for the project"""
 
+class User():
+    """User class. This class is an object for all users, and contains
+    all of a user's attributes and methods, as well as the constructor
+    for a User object"""
+
+    def __init__(self, email, password, name, dob, address, role):
+        """Constructor for User class"""
+        self.email = email
+        self.password = password
+        self.name = name
+        self.dob = dob
+        self.address = address
+        self.role = role
