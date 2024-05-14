6f7aaac8c50487b458d4906d80a265d1ae7955bb
alexlines8 <alexlines8@yahoo.co.uk>
2024-05-14 16:52:10 +0100

    Minor email changes

--- email_folder/views.py
+++ email_folder/views.py
@@ -1,16 +1,14 @@
 import smtplib
 
-from models import User
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
 
 from_address = "FeedForwardUK@gmail.com"
 password = "okgx wpwq xvio ncei"
 
 
 def send_welcome_email(User):
+    """Function to send a welcome email when a user registers"""
     contents = "Hi " + User.get_first_name()
     contents += "\n Welcome to FeedForward! Thank you for signing up."
 
@@ -27,7 +25,3 @@ def send_welcome_email(User):
     text = msg.as_string()
     s.sendmail(from_address, User.get_email(), text)
     s.quit()
-
-
-new_user = User("alex8lines@gmail.com", "test", "alex", "lines", "08/04/2004", "test", "user")
-send_welcome_email(new_user)
