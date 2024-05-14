9d930d4f8150494e052c6514998e213307c56285
alexlines8 <alexlines8@yahoo.co.uk>
2024-05-14 16:39:17 +0100

    Add code for welcome email

--- email_folder/views.py
+++ email_folder/views.py
@@ -1,7 +1,33 @@
-from models import User
+import smtplib
 
+from models import User
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 from_address = "FeedForwardUK@gmail.com"
+password = "okgx wpwq xvio ncei"
+
+
 def send_welcome_email(User):
     contents = "Hi " + User.get_first_name()
-    contents += "\n Welcome to FeedForward!"
+    contents += "\n Welcome to FeedForward! Thank you for signing up."
+
+    msg = MIMEMultipart()
+    msg['From'] = from_address
+    msg['To'] = User.get_email()
+    msg["Subject"] = "Welcome to FeedForward"
+    body = contents
+    msg.attach(MIMEText(body, 'plain'))
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(from_address, password)
+    text = msg.as_string()
+    s.sendmail(from_address, User.get_email(), text)
+    s.quit()
+
+
+new_user = User("alex8lines@gmail.com", "test", "alex", "lines", "08/04/2004", "test", "user")
+send_welcome_email(new_user)
