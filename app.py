800220bd1a405611020d4508f1d22889439175d4
Suryansh <c2064746@newcastle.ac.uk>
2024-05-16 13:59:42 +0100

    revision

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -14,6 +15,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_ECHO'] = os.getenv('SQLALCHEMY_ECHO') == 'True'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS') == 'True'
 db = SQLAlchemy(app)
+
+# initialise instance of LoginManager
+login_manager = LoginManager()
+# set view function which renders login page
+login_manager.login_view = 'users.login'
+# register LoginManager instance with app
+login_manager.init_app(app)
+
+# import User from models (imported here to avoid Circular Import Error)
+
+
+
+@login_manager.user_loader
+def load_user(email):
+    from models import User
+    """ user loader function for LoginManager to get user instances from the db """
+    return User.query.filter_by(email=email).first()
 
 
 # Define your Flask route to render the HTML template
@@ -56,9 +74,21 @@ def newsletter():
 def create_admin_account():
     return render_template('main/create_admin_account.html')
 
+
 @app.route('/create_advert')
 def create_advert():
     return render_template('main/createadvert.html')
 
+
+
+
+
 if __name__ == '__main__':
+    # Import blueprints (imported here to avoid Circular Import Error)
+    from users.views import users_blueprint
+    from admin.views import admin_blueprint
+
+    # Register blueprints with app
+    app.register_blueprint(users_blueprint)
+    app.register_blueprint(admin_blueprint)
     app.run(debug=True)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from flask_login import login_user, logout_user, current_user, login_required
-from users.forms import RegisterForm, LoginForm
+from users.forms import SignUpForm, LoginForm
 import bcrypt
 from flask import Blueprint, flash, render_template, session, redirect, url_for
 from models import User
@@ -11,7 +11,7 @@ users_blueprint = Blueprint('users', __name__, template_folder='templates')
 @users_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
     # create signup form object
-    form = RegisterForm()
+    form = SignUpForm()
     # Check if user is logged out
     if current_user.is_anonymous:
         # if request method is POST or form is valid
@@ -48,4 +48,3 @@ def register():
         return render_template('main/account.html')
     # if request method is GET or form not valid re-render signup page
     return render_template('main/signup.html', form=form)
-
