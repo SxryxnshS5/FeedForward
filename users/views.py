97c1f021ae684b08586de091aa51420c2977280d
Rebecca Picken <c2013042@ncl.ac.uk>
2024-05-17 17:51:35 +0100

    fix db bug in signup

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from users.forms import SignUpForm, LoginForm
 import bcrypt
 from flask import Blueprint, flash, render_template, session, redirect, url_for
 from models import User
-from app import db
+from app import db, app
 from markupsafe import Markup
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
@@ -18,32 +18,34 @@ def signup():
     if current_user.is_anonymous:
         # if request method is POST or form is valid
         if form.validate_on_submit():
-            print("PLEASE")
-            user = User.query.filter_by(email=form.email.data).first()
-            # if this returns a user, then the email already exists in database
-            # if email already exists redirect user back to signup page with error message so user can try again
-            if user:
-                flash('Email address already exists')
-                return render_template('main/signup.html', form=form)
+            with app.app_context():
+                print("PLEASE")
+                user = User.query.filter_by(email=form.email.data).first()
+                # if this returns a user, then the email already exists in database
+                # if email already exists redirect user back to signup page with error message so user can try again
+                if user:
+                    flash('Email address already exists')
+                    return render_template('main/signup.html', form=form)
 
-            # create a new user with the form data
-            new_user = User(email=form.email.data,
+                # create a new user with the form data
+                new_user = User(email=form.email.data,
                             first_name=form.first_name.data,
                             surname=form.last_name.data,
                             password=form.password.data,
                             role='user',
                             dob=form.dob.data,
-                            address=form.address.data)
+                            address=form.address.data,
+                            phone=form.phone.data)
 
-            # add the new user to the database
+                # add the new user to the database
 
-            db.session.add(new_user)
-            db.session.commit()
+                db.session.add(new_user)
+                db.session.commit()
 
-            # create session variable
-            session['email'] = new_user.email
-            # sends user to 2fa page
-            return redirect(url_for('main/account.html'))
+                # create session variable
+                session['email'] = new_user.email
+                # sends user to 2fa page
+                return redirect(url_for('main/account.html'))
     else:
         # if user is already logged in
         flash('You are already logged in.')
