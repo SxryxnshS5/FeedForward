068d706b0daccc44e2d87d613ff063efc0938f4b
alexlines8 <63907153+alexlines8@users.noreply.github.com>
2024-05-19 15:16:16 +0100

    test

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template
 from dotenv import load_dotenv
 from extensions import init_app, db, login_manager, csrf
 
-
 app = Flask(__name__)
 
 # Configuring the secret key to sign and validate session cookies.
@@ -20,34 +19,42 @@ def load_user(id):
     """ user loader function for LoginManager to get user instances from the db """
     return User.query.get(int(id))
 
+
 # Define your Flask routes to render the HTML templates
 @app.route('/')
 def index():
     return render_template('main/index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('main/about.html')
+
 
 @app.route('/adminaccount')
 def adminaccount():
     return render_template('main/adminaccount.html')
 
+
 @app.route('/newsletter')
 def newsletter():
     return render_template('main/newsletter.html')
+
 
 @app.route('/create_admin_account')
 def create_admin_account():
     return render_template('main/create_admin_account.html')
 
+
 @app.route('/messages')
 def messages():
     return render_template('main/messages.html')
 
+
 @app.route('/chat')
 def chat():
     return render_template('main/chat.html')
+
 
 if __name__ == '__main__':
     # Import blueprints (imported here to avoid Circular Import Error)
