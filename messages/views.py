b90cdad971b86d5522546df3af12473073e1f576
Rebecca Picken <c2013042@ncl.ac.uk>
2024-05-28 13:49:43 +0100

    Add message advertiser functionality & start work on live updates

--- messages/views.py
+++ messages/views.py
@@ -3,10 +3,8 @@ from flask_login import login_user, logout_user, current_user, login_required
 import datalink
 from messages.forms import MessageForm
 
-from flask import Blueprint, flash, render_template, session, redirect, url_for
+from flask import Blueprint, flash, render_template, jsonify, session
 from models import User, Message
-from app import db, app
-from markupsafe import Markup
 from datetime import datetime
 
 messages_blueprint = Blueprint('messages', __name__, template_folder='templates')
@@ -49,9 +47,10 @@ def view_messages():
                            recent_messages=recent_messages, time_messages=time_messages)
 
 
-
 @messages_blueprint.route('/<int:id>/chat', methods=['GET', 'POST'])
 def chat(id):
+    # store messanger id to be used for updating messages later with JS
+    session['messager'] = id
     form = MessageForm()
     # send new message if POST
     if form.validate_on_submit():
@@ -62,4 +61,10 @@ def chat(id):
     messages = datalink.get_message_history(current_user.id, id)
     user = datalink.get_user_from_id(id)
     name = user.first_name
-    return render_template('main/chat.html',form=form, conversation=messages, name=name)
+    return render_template('main/chat.html', form=form, conversation=messages, name=name)
+
+@messages_blueprint.route('/update_chat', methods=['POST'])
+def update_chat():
+    id = session['messager']
+    messages = datalink.get_message_history(current_user.id, id)
+    return jsonify({'messages': render_template('update_message.html', conversation=messages)})
